867bce102c04bf34cb9eb98fe966ab732a657b02
Murat Yaşar <murat.yasar@comkom.de>
2025-07-04 13:29:10 +0200

    Add docker

---
+++ edureka/GitLab/ci-cd.sh
@@ -0,0 +1,14 @@
+# Install pre-requisites
+sudo apt-get install openssh-server
+sudo apt install postfix -y
+# Set system mail for postfix
+
+
+# Download and Install GitLab
+# Add your host-name to GitLab
+nano /etc/gitlab/gitlab.rb
+#===>
+# external_url: 'http://www.dev-lhg.comkom.de'
+
+
+
