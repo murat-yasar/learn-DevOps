0786a917c2454a15a6efb97e6aae523c702c9675
Murat Yaşar <dev.muratyasar@gmail.com>
2025-08-28 12:54:06 +0200

    learn: multi provider

--- KodeKloud/terraform/main.tf
+++ KodeKloud/terraform/main.tf
@@ -7,3 +7,14 @@ resource "local_file" "cat" {
    filename = "/root/cat.txt"
    content = "My favourite pet is my cat!"
 }
+
+resource "local_file" "my-cat" {
+   filename = "/root/my-cat"
+   content = "My cat's name is kitty!"
+}
+
+resource "random_pet" "other-pet" {
+    length = "1"
+    prefix = "Mr"
+    separator = "."
+}
---
+++ KodeKloud/terraform/terraform.sh
@@ -0,0 +1,3 @@
+terraform init                # initialize terraform
+terraform plan                # 
+terraform apply               # applies the .tf file
