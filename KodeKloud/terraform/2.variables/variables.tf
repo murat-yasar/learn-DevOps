7362f4413bae4c7cb142ecc3dc59c98093f3e3bc
Murat Yaşar <dev.muratyasar@gmail.com>
2025-08-28 13:47:26 +0200

    Terraform: block variables

--- KodeKloud/terraform/2.variables/variables.tf
+++ KodeKloud/terraform/2.variables/variables.tf
@@ -1,19 +1,37 @@
 variable "filename" {
    default = "/root/pets.txt"
+   type = string
+   description = "the path of local file"
 }
 
-variable "content" {
-   default = "We love pets!"
+variable file-content {
+   type = map(string)
+   default = {
+      "statement1" = "We love pets!"
+      "statement2" = "We love animals!"
+   }
+   description = "the content of the file"
 }
 
 variable "prefix" {
-   default = "Mr"
+   default = ["Mr", "Mrs", "Dr"]
+   type = list(string)
+   description = "the prefix to be set"
 }
 
 variable "separator" {
    default = "."
+   type = string
 }
 
 variable "length" {
-   default = "1"
+   default = 2
+   type = number
+   description = "the length of the pet name"
+}
+
+variable "password_changed" {
+   default = false
+   type = bool
+   description = "has the password been changed"
 }
--- KodeKloud/terraform/terraform.sh
+++ KodeKloud/terraform/terraform.sh
@@ -1,3 +1,3 @@
-terraform init                # initialize terraform
-terraform plan                # 
-terraform apply               # applies the .tf file
+terraform init    # Initialize Terraform working directory and download required providers/modules
+terraform plan    # Create an execution plan showing what changes Terraform will make
+terraform apply   # Apply the changes required to reach the desired infrastructure state
