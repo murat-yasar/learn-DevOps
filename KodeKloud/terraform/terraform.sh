fdab984a1d4fd2e98800e81410d25cbc57129ac7
Murat Yaşar <dev.muratyasar@gmail.com>
2025-08-28 14:54:37 +0200

    terraform: resource attributes

--- KodeKloud/terraform/terraform.sh
+++ KodeKloud/terraform/terraform.sh
@@ -1,3 +1,4 @@
 terraform init    # Initialize Terraform working directory and download required providers/modules
 terraform plan    # Create an execution plan showing what changes Terraform will make
 terraform apply   # Apply the changes required to reach the desired infrastructure state
+terraform show    # Inspect the current state or saved plan file
